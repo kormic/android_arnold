a9111261dfb78c246d04667febecebff60df5c06
Michalis Kornelakis <michalis@cyberlogic.gr>
2018-04-02 09:55:35 +0300

    Change the colorControlHighlight for the MoreMenu theme Add a toolbar in my body screen Add the more menu in my body's toolbar

--- mobile/src/main/java/gr/komic/arnold/BodyActivity.java
+++ mobile/src/main/java/gr/komic/arnold/BodyActivity.java
@@ -2,6 +2,7 @@ package gr.komic.arnold;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class BodyActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class BodyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_body);
+
+        Toolbar mToolbar = findViewById(R.id.toolbar);
+        mToolbar.inflateMenu(R.menu.more_menu);
     }
 }
