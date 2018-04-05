76533b39c9b00f9bd7321653d2f361c9cb54bd04
Michael Kornelakis <michalis@cyberlogic.gr>
2018-04-05 18:01:58 +0300

    Add a layout to use for the spinners to centre their text Move the code that setup the spinners in their own function Set the gender and workouts per week spinners using the SpinnersHelper class

--- mobile/src/main/java/gr/komic/arnold/BodyActivity.java
+++ mobile/src/main/java/gr/komic/arnold/BodyActivity.java
@@ -10,9 +10,11 @@ import gr.komic.arnold.helpers.SpinnersHelper;
 
 public class BodyActivity extends AppCompatActivity {
 
+    Spinner genderSpinner;
     Spinner ageSpinner;
     Spinner weightSpinner;
     Spinner heightSpinner;
+    Spinner workoutsPerWeekSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +25,28 @@ public class BodyActivity extends AppCompatActivity {
         mToolbar.inflateMenu(R.menu.more_menu);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        this.setupSpinners();
+    }
+
+    private void setupSpinners() {
+        genderSpinner = findViewById(R.id.gender_spinner);
+        ArrayAdapter<String> genderSpinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_text_center, SpinnersHelper.getGenderValues());
+        genderSpinner.setAdapter(genderSpinnerAdapter);
 
         ageSpinner = findViewById(R.id.age_spinner);
-        ArrayAdapter<Integer> ageSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, SpinnersHelper.getAgeValues());
+        ArrayAdapter<Integer> ageSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.spinner_text_center, SpinnersHelper.getAgeValues());
         ageSpinner.setAdapter(ageSpinnerAdapter);
 
         weightSpinner = findViewById(R.id.weight_spinner);
-        ArrayAdapter<Integer> weightSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, SpinnersHelper.getWeightValues());
+        ArrayAdapter<Integer> weightSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.spinner_text_center, SpinnersHelper.getWeightValues());
         weightSpinner.setAdapter(weightSpinnerAdapter);
 
         heightSpinner = findViewById(R.id.height_spinner);
-        ArrayAdapter<Integer> heightSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.support_simple_spinner_dropdown_item, SpinnersHelper.getHeightValues());
+        ArrayAdapter<Integer> heightSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.spinner_text_center, SpinnersHelper.getHeightValues());
         heightSpinner.setAdapter(heightSpinnerAdapter);
+
+        workoutsPerWeekSpinner = findViewById(R.id.workouts_per_week_spinner);
+        ArrayAdapter<Integer> workoutsPerWeekSpinnerAdapter = new ArrayAdapter<Integer>(this, R.layout.spinner_text_center, SpinnersHelper.getWorkoutsPerWeekValues());
+        workoutsPerWeekSpinner.setAdapter(workoutsPerWeekSpinnerAdapter);
     }
 }
--- mobile/src/main/java/gr/komic/arnold/helpers/SpinnersHelper.java
+++ mobile/src/main/java/gr/komic/arnold/helpers/SpinnersHelper.java
@@ -2,6 +2,13 @@ package gr.komic.arnold.helpers;
 
 public class SpinnersHelper {
 
+    public static String[] getGenderValues() {
+        String[] genderValues = new String[2];
+        genderValues[0] = "Male";
+        genderValues[1] = "Female";
+        return genderValues;
+    }
+
     public static Integer[] getAgeValues() {
         Integer[] ages = new Integer[88];
         for(int i = 0; i < ages.length; i++) {
@@ -24,5 +31,13 @@ public class SpinnersHelper {
             heights[i] = 130 + i;
         }
         return heights;
+    }
+
+    public static Integer[] getWorkoutsPerWeekValues() {
+        Integer[] workoutsPerWeekValues = new Integer[7];
+        for(int i = 0; i < workoutsPerWeekValues.length; i++) {
+            workoutsPerWeekValues[i] = i + 1;
+        }
+        return workoutsPerWeekValues;
     }
 }
