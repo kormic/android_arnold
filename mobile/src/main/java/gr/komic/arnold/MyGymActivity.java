1834c31cae6a37c7065cbd9c0fe3cc15e42a6829
Michael Kornelakis <ycamic@gmail.com>
2018-04-12 22:26:26 +0300

    Add a DatePickerFragment class Create the datepicker Implement the onDateSetListener in my gym activity Show the datepicker and set the registration date Move the autocomplete's setup code in its own function

--- mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
+++ mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
@@ -1,12 +1,15 @@
 package gr.komic.arnold;
 
+import android.app.DatePickerDialog;
 import android.support.annotation.NonNull;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -21,7 +24,7 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
-public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener, DatePickerDialog.OnDateSetListener {
 
     private GoogleApiClient mGoogleApiClient;
     PlaceAutocompleteFragment autocompleteFragment;
@@ -46,7 +49,47 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         this.setupViews();
+        this.setupAutocomplete();
+    }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
+    // handle connection failures.
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.my_gym_registration || view.getId() == R.id.my_gym_registration_date_picker) {
+            DialogFragment datePickerFragment = new DatePickerFragment();
+            datePickerFragment.show(getSupportFragmentManager(), "datePickerFragment");
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int day) {
+        String dateString = day + "/" + (month + 1) + "/" + year;
+        this.mGymRegistrationTextView.setText(dateString);
+    }
+
+    private void setupViews() {
+        this.mGymNameEditext = findViewById(R.id.my_gym_name);
+        this.mGymAddressEditText = findViewById(R.id.my_gym_address);
+        this.mGymPhoneEditText = findViewById(R.id.my_gym_phone);
+        this.mGymRatingEditText = findViewById(R.id.my_gym_rating);
+        this.mGymWebsiteEditText = findViewById(R.id.my_gym_website);
+        this.mGymRegistrationTextView = findViewById(R.id.my_gym_registration);
+        this.mRegistrationDateImageButton = findViewById(R.id.my_gym_registration_date_picker);
+        this.mSubscriptionEditText = findViewById(R.id.my_gym_subscription);
+
+        this.mRegistrationDateImageButton.setOnClickListener(this);
+        this.mGymRegistrationTextView.setOnClickListener(this);
+    }
+
+    private void setupAutocomplete() {
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
                 .addApi(Places.GEO_DATA_API)
@@ -66,9 +109,6 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
                 Log.i("OH 3", "PhoneNumber " + place.getPhoneNumber());
                 Log.i("OH 4", "Rating " + place.getRating());
                 Log.i("OH 5", "Website " + place.getWebsiteUri());
-
-
-
             }
 
             @Override
@@ -79,33 +119,4 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
         });
     }
 
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-    }
-
-    // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
-    // handle connection failures.
-
-    private void setupViews() {
-        this.mGymNameEditext = findViewById(R.id.my_gym_name);
-        this.mGymAddressEditText = findViewById(R.id.my_gym_address);
-        this.mGymPhoneEditText = findViewById(R.id.my_gym_phone);
-        this.mGymRatingEditText = findViewById(R.id.my_gym_rating);
-        this.mGymWebsiteEditText = findViewById(R.id.my_gym_website);
-        this.mGymRegistrationTextView = findViewById(R.id.my_gym_registration);
-        this.mRegistrationDateImageButton = findViewById(R.id.my_gym_registration_date_picker);
-        this.mSubscriptionEditText = findViewById(R.id.my_gym_subscription);
-
-        this.mRegistrationDateImageButton.setOnClickListener(this);
-        this.mGymRegistrationTextView.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-        if(view.getId() == R.id.my_gym_registration || view.getId() == R.id.my_gym_registration_date_picker) {
-            Toast.makeText(MyGymActivity.this, "Registration clicked",Toast.LENGTH_SHORT).show();
-        }
-    }
 }
