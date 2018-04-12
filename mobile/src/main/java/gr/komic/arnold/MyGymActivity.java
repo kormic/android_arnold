9773f710c23b51b793dfd2f788f14bdb7fc98911
Michael Kornelakis <ycamic@gmail.com>
2018-04-12 22:00:06 +0300

    Fix the registration's textview id Add clickable and focusable to the registration textview and the registration date picker Setup the views Implement the onClickListener Add click listeners to the registration textview and imagebutton

--- mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
+++ mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
@@ -6,6 +6,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -16,10 +21,19 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
-public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
     private GoogleApiClient mGoogleApiClient;
     PlaceAutocompleteFragment autocompleteFragment;
+
+    EditText mGymNameEditext;
+    EditText mGymAddressEditText;
+    EditText mGymPhoneEditText;
+    EditText mGymRatingEditText;
+    EditText mGymWebsiteEditText;
+    TextView mGymRegistrationTextView;
+    ImageButton mRegistrationDateImageButton;
+    EditText mSubscriptionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +44,8 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
         mToolbar.inflateMenu(R.menu.more_menu);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.setupViews();
 
         mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
@@ -64,7 +80,6 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
     }
 
 
-
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
@@ -72,4 +87,25 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
 
     // TODO: Please implement GoogleApiClient.OnConnectionFailedListener to
     // handle connection failures.
+
+    private void setupViews() {
+        this.mGymNameEditext = findViewById(R.id.my_gym_name);
+        this.mGymAddressEditText = findViewById(R.id.my_gym_address);
+        this.mGymPhoneEditText = findViewById(R.id.my_gym_phone);
+        this.mGymRatingEditText = findViewById(R.id.my_gym_rating);
+        this.mGymWebsiteEditText = findViewById(R.id.my_gym_website);
+        this.mGymRegistrationTextView = findViewById(R.id.my_gym_registration);
+        this.mRegistrationDateImageButton = findViewById(R.id.my_gym_registration_date_picker);
+        this.mSubscriptionEditText = findViewById(R.id.my_gym_subscription);
+
+        this.mRegistrationDateImageButton.setOnClickListener(this);
+        this.mGymRegistrationTextView.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.my_gym_registration || view.getId() == R.id.my_gym_registration_date_picker) {
+            Toast.makeText(MyGymActivity.this, "Registration clicked",Toast.LENGTH_SHORT).show();
+        }
+    }
 }
