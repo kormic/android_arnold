657cfb11954d7333bce5eaaa1ad8907e55f2fd54
Michael Kornelakis <ycamic@gmail.com>
2018-04-12 21:23:10 +0300

    Design and add my gym xmls Add the setOnPlaceSelectedListener on autocomplete fragment

--- mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
+++ mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
@@ -5,15 +5,21 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.GeoDataClient;
+import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 
 public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleApiClient mGoogleApiClient;
+    PlaceAutocompleteFragment autocompleteFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,7 +37,33 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
                 .addApi(Places.PLACE_DETECTION_API)
                 .enableAutoManage(this, this)
                 .build();
+
+        this.autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i("OH JESUS", "Place: " + place.getName());
+                Log.i("OH 2", "Address " + place.getAddress());
+                Log.i("OH 3", "PhoneNumber " + place.getPhoneNumber());
+                Log.i("OH 4", "Rating " + place.getRating());
+                Log.i("OH 5", "Website " + place.getWebsiteUri());
+
+
+
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("OH MY GOD!", "An error occurred: " + status);
+            }
+        });
     }
+
+
 
     @Override
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
