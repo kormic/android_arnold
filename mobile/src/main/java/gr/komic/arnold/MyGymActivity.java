521970de2a730739107c11c76fed8092465f995b
Michael Kornelakis <michalis@cyberlogic.gr>
2018-04-13 11:56:25 +0300

    Store my gym info in shared preferences Handle the editing of the inputs

--- mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
+++ mobile/src/main/java/gr/komic/arnold/MyGymActivity.java
@@ -1,33 +1,48 @@
 package gr.komic.arnold;
 
 import android.app.DatePickerDialog;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.GeoDataClient;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
+import com.google.gson.Gson;
 
-public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener, DatePickerDialog.OnDateSetListener, PlaceSelectionListener {
+import gr.komic.arnold.Models.MyGymInfo;
+import gr.komic.arnold.helpers.Constants;
+
+public class MyGymActivity extends AppCompatActivity
+        implements
+        GoogleApiClient.OnConnectionFailedListener,
+        View.OnClickListener,
+        DatePickerDialog.OnDateSetListener,
+        PlaceSelectionListener {
 
     private GoogleApiClient mGoogleApiClient;
     PlaceAutocompleteFragment autocompleteFragment;
+
+    Gson gson = new Gson();
+    String myGymInfoJson;
+    SharedPreferences mSharedPrefs;
+    SharedPreferences.Editor mEditor;
+    private MyGymInfo myGymInfo;
 
     EditText mGymNameEditext;
     EditText mGymAddressEditText;
@@ -35,8 +50,8 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
     EditText mGymRatingEditText;
     EditText mGymWebsiteEditText;
     TextView mGymRegistrationTextView;
-    ImageButton mRegistrationDateImageButton;
-    EditText mSubscriptionEditText;
+    ImageButton mGymRegistrationDateImageButton;
+    EditText mGymSubscriptionEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +63,7 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        this.restoreMyGymInfo();
         this.setupViews();
         this.setupAutocomplete();
     }
@@ -72,21 +88,58 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
     @Override
     public void onDateSet(DatePicker view, int year, int month, int day) {
         String dateString = day + "/" + (month + 1) + "/" + year;
-        this.mGymRegistrationTextView.setText(dateString);
+        this.myGymInfo.setRegistrationDateString(dateString);
+        this.mGymRegistrationTextView.setText(this.myGymInfo.getRegistrationDateString());
+        this.storeMyGymInfo();
     }
 
     @Override
     public void onPlaceSelected(Place place) {
-        this.mGymNameEditext.setText(place.getName() != null ? place.getName(): "Not available");
-        this.mGymAddressEditText.setText(place.getAddress() != null ? place.getAddress(): "Not available");
-        this.mGymPhoneEditText.setText(place.getPhoneNumber() != null ? place.getPhoneNumber(): "Not available");
-        this.mGymRatingEditText.setText(String.valueOf(place.getRating()));
-        this.mGymWebsiteEditText.setText(place.getWebsiteUri() != null ? place.getWebsiteUri().toString() : "Not available");
+        this.myGymInfo.setName(place.getName() != null ? place.getName().toString() : "Not available");
+        this.mGymNameEditext.setText(this.myGymInfo.getName());
+
+        this.myGymInfo.setAddress(place.getAddress() != null ? place.getAddress().toString() : "Not available");
+        this.mGymAddressEditText.setText(this.myGymInfo.getAddress());
+
+        this.myGymInfo.setPhone(place.getPhoneNumber() != null ? place.getPhoneNumber().toString() : "Not available");
+        this.mGymPhoneEditText.setText(this.myGymInfo.getPhone());
+
+        this.myGymInfo.setRating(place.getRating());
+        this.mGymRatingEditText.setText(String.valueOf(this.myGymInfo.getRating()));
+
+        this.myGymInfo.setWebsite(place.getWebsiteUri() != null ? place.getWebsiteUri().toString() : "Not available");
+        this.mGymWebsiteEditText.setText(this.myGymInfo.getWebsite());
+
+        this.myGymInfo.setRegistrationDateString("");
+        this.mGymRegistrationTextView.setText(this.myGymInfo.getRegistrationDateString());
+
+        this.myGymInfo.setSubscription(0);
+        this.mGymSubscriptionEditText.setText("");
+
+        this.storeMyGymInfo();
     }
 
     @Override
     public void onError(Status status) {
         // TODO: Handle the error.
+    }
+
+    private void storeMyGymInfo() {
+        this.myGymInfoJson = gson.toJson(this.myGymInfo);
+        this.mEditor.putString(Constants.MY_GYM_INFO_OBJECT, this.myGymInfoJson);
+        this.mEditor.commit();
+    }
+
+    private void restoreMyGymInfo() {
+        this.mSharedPrefs = this.getSharedPreferences(Constants.MY_GYM_INFO, MODE_PRIVATE);
+        this.mEditor = mSharedPrefs.edit();
+
+        String storedMyGymInfoJson = this.mSharedPrefs.getString(Constants.MY_GYM_INFO_OBJECT, null);
+        if(storedMyGymInfoJson != null) {
+            this.myGymInfo = gson.fromJson(storedMyGymInfoJson, MyGymInfo.class);
+        }else {
+            this.myGymInfo = new MyGymInfo();
+        }
     }
 
     private void setupViews() {
@@ -96,15 +149,49 @@ public class MyGymActivity extends AppCompatActivity implements GoogleApiClient.
         this.mGymRatingEditText = findViewById(R.id.my_gym_rating);
         this.mGymWebsiteEditText = findViewById(R.id.my_gym_website);
         this.mGymRegistrationTextView = findViewById(R.id.my_gym_registration);
-        this.mRegistrationDateImageButton = findViewById(R.id.my_gym_registration_date_picker);
-        this.mSubscriptionEditText = findViewById(R.id.my_gym_subscription);
+        this.mGymRegistrationDateImageButton = findViewById(R.id.my_gym_registration_date_picker);
+        this.mGymSubscriptionEditText = findViewById(R.id.my_gym_subscription);
 
-        this.mRegistrationDateImageButton.setOnClickListener(this);
+        this.mGymRegistrationDateImageButton.setOnClickListener(this);
         this.mGymRegistrationTextView.setOnClickListener(this);
+        this.mGymSubscriptionEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try {
+                    if(editable.length() > 0){
+                        float subscriptionValue = Float.parseFloat(editable.toString());
+                        MyGymActivity.this.myGymInfo.setSubscription(subscriptionValue);
+                    }else {
+                        MyGymActivity.this.myGymInfo.setSubscription(0);
+                    }
+                }catch (NumberFormatException e) {
+                    MyGymActivity.this.myGymInfo.setSubscription(0);
+                }
+
+                MyGymActivity.this.storeMyGymInfo();
+            }
+        });
+
+
+        this.mGymNameEditext.setText(this.myGymInfo.getName());
+        this.mGymAddressEditText.setText(this.myGymInfo.getAddress());
+        this.mGymPhoneEditText.setText(this.myGymInfo.getPhone());
+        this.mGymRatingEditText.setText(String.valueOf(this.myGymInfo.getRating()));
+        this.mGymWebsiteEditText.setText(this.myGymInfo.getWebsite());
+        this.mGymRegistrationTextView.setText(this.myGymInfo.getRegistrationDateString());
+        this.mGymSubscriptionEditText.setText(String.format("%.2f",this.myGymInfo.getSubscription()));
     }
 
     private void setupAutocomplete() {
-        mGoogleApiClient = new GoogleApiClient
+        this.mGoogleApiClient = new GoogleApiClient
                 .Builder(this)
                 .addApi(Places.GEO_DATA_API)
                 .addApi(Places.PLACE_DETECTION_API)
--- mobile/src/main/java/gr/komic/arnold/helpers/Constants.java
+++ mobile/src/main/java/gr/komic/arnold/helpers/Constants.java
@@ -3,6 +3,8 @@ package gr.komic.arnold.helpers;
 public class Constants {
     public static final String BODY_INFO = "bodyInfo";
     public static final String USER_INFO_OBJECT = "userInfoObject";
+    public static final String MY_GYM_INFO = "myGymInfo";
+    public static final String MY_GYM_INFO_OBJECT = "myGymInfoObject";
 
 //    LOGGING TAGS
     public static final String HOME_ACTIVITY = "HomeActivity";
