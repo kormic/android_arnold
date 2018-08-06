866ba640775f3615b93b2bcb716b8e3ebfc727a2
Michael Kornelakis <ycamic@gmail.com>
2018-08-06 21:23:00 +0300

    Add a constructor and getters / setters to Progress class

--- mobile/src/main/java/gr/komic/arnold/Models/Progress.java
+++ mobile/src/main/java/gr/komic/arnold/Models/Progress.java
@@ -5,4 +5,43 @@ public class Progress {
     private float wristDiameter = 0;
     private float neckDiameter = 0;
     private float waistDiameter = 0;
+
+    public Progress(String dateAddedString, float wristDiameter, float neckDiameter, float waistDiameter) {
+        this.dateAddedString = dateAddedString;
+        this.wristDiameter = wristDiameter;
+        this.neckDiameter = neckDiameter;
+        this.waistDiameter = waistDiameter;
+    }
+
+    public String getDateAddedString() {
+        return dateAddedString;
+    }
+
+    public void setDateAddedString(String dateAddedString) {
+        this.dateAddedString = dateAddedString;
+    }
+
+    public float getWristDiameter() {
+        return wristDiameter;
+    }
+
+    public void setWristDiameter(float wristDiameter) {
+        this.wristDiameter = wristDiameter;
+    }
+
+    public float getNeckDiameter() {
+        return neckDiameter;
+    }
+
+    public void setNeckDiameter(float neckDiameter) {
+        this.neckDiameter = neckDiameter;
+    }
+
+    public float getWaistDiameter() {
+        return waistDiameter;
+    }
+
+    public void setWaistDiameter(float waistDiameter) {
+        this.waistDiameter = waistDiameter;
+    }
 }
