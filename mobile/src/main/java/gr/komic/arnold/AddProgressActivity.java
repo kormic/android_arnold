31be91f1bf99d72128ccf59929b7413e61ab4cf6
Michael Kornelakis <ycamic@gmail.com>
2018-08-19 19:21:52 +0300

    Remove unused imports in add progress activity

--- mobile/src/main/java/gr/komic/arnold/AddProgressActivity.java
+++ mobile/src/main/java/gr/komic/arnold/AddProgressActivity.java
@@ -1,6 +1,5 @@
 package gr.komic.arnold;
 
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
