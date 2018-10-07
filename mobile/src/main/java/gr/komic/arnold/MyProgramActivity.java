8673aa780bdaf25079329e47ad7dd4d3944c9a55
Michael Kornelakis <ycamic@gmail.com>
2018-10-07 22:23:32 +0300

    Connect the floating button to a listener

--- mobile/src/main/java/gr/komic/arnold/MyProgramActivity.java
+++ mobile/src/main/java/gr/komic/arnold/MyProgramActivity.java
@@ -1,10 +1,16 @@
 package gr.komic.arnold;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
-public class MyProgramActivity extends AppCompatActivity {
+public class MyProgramActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MyProgramActivity";
+
+    FloatingActionButton addProgramFab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +21,16 @@ public class MyProgramActivity extends AppCompatActivity {
         mToolbar.inflateMenu(R.menu.more_menu);
         setSupportActionBar(mToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        this.addProgramFab = findViewById(R.id.add_prograrm_fab);
+        this.addProgramFab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.add_prograrm_fab:
+                Log.d(TAG, "Add program clicked");
+        }
     }
 }
